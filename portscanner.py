e79285a68debd8fc6ec7c1467772a64bcc6ffd7d
Devon Lumsden <devonlumsden@mail.com>
2020-11-27 10:40:37 +1300

    add check_ip

--- portscanner.py
+++ portscanner.py
@@ -1,6 +1,14 @@
 import socket
 from IPy import IP
 
+def check_ip(ip):
+    #check if value given is IP or domainname 
+    try:
+        IP(ip)
+        return(ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+
 def scan_port(ipaddress, port):
     try:
         sock = socket.socket()
@@ -11,7 +19,8 @@ def scan_port(ipaddress, port):
     except: 
         print('[-] Port ' + str(port) +  ' is Closed')
 
-ipaddress = input('[+] Enter Target To Scan')
+ipaddress = input('[+] Enter Target To Scan: ')
+converted_ip = check_ip(ipaddress)
 
 for port in range(50, 90):
-    scan_port(ipaddress, port)
+    scan_port(converted_ip, port)
