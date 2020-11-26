1f25c606bc52c5ada008a5d13db0cddd856179e4
Devon Lumsden <devonlumsden@mail.com>
2020-11-27 10:25:55 +1300

    add basic portscan

--- portscanner.py
+++ portscanner.py
@@ -1,2 +1,13 @@
 import socket
 from IPy import IP
+
+ipaddress = input('[+] Enter Target To Scan')
+port = 80
+
+try:
+    sock = socket.socket()
+    #establish connection with target machine
+    sock.connect((ipaddress, port))
+    print('[+] Port 80 is Open')
+except: 
+    print('[-] Port 80 is Closed')
