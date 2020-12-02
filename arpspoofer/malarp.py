f7f89ad0b16f303730480eda0847d73c0e14d01f
Devon Lumsden <devonlumsden@mail.com>
2020-12-03 09:24:06 +1300

    add malarp.py & display mac address

---
+++ arpspoofer/malarp.py
@@ -0,0 +1,18 @@
+from scapy.all import * 
+import sys
+import time
+
+def get_mac_address(ip_address):
+    broadcast_layer = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    arp_layer = scapy.ARP(pdst=ip_address)
+    get_mac_packet = broadcast_layer/arp_layer
+    answer = scapy.srp(get_mac_packet, timeout=2, verbose=False)[0]
+    return answer[0][1].hwsrc
+
+target_ip = str(sys.argv[2])
+router_ip = str(sys.argv[1])
+target_mac = str(get_mac_address(target_ip))
+router_mac = str(get_mac_address(router_ip))
+
+print(target_mac)
+print(router_mac)
